3fdff86f658f3d5998b8b65baa2f70e8af83571d
Sean Ford <seanford@protonmail.com>
2025-10-24 08:04:38 -0700

    feat: get human choice

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
   return choices[randomNum];
 }
+
+function getHumanChoice() {
+  // Odin Project states we do not need to validate the input
+  const choice = prompt("Enter either 'rock', 'paper', or 'scissors'");
+  return choice;
+}
