403dad585f324dc732d2e1fab012a84702b9d73a
Sean Ford <seanford@protonmail.com>
2025-10-24 08:27:44 -0700

    fix: standardize format of choices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
   const randomNum = Math.floor(Math.random() * 3);
   return choices[randomNum];
 }
@@ -7,5 +7,5 @@ function getComputerChoice() {
 function getHumanChoice() {
   // Odin Project states we do not need to validate the input
   const choice = prompt("Enter either 'rock', 'paper', or 'scissors'");
-  return choice;
+  return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
 }
